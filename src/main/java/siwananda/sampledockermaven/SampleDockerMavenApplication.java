80852766ab090d8ce24f0c6168d2e2a40871e5af
Wira Siwananda <siwananda@gmail.com>
2020-09-30 21:55:55 +0800

    first commit

---
+++ src/main/java/siwananda/sampledockermaven/SampleDockerMavenApplication.java
@@ -0,0 +1,13 @@
+package siwananda.sampledockermaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDockerMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleDockerMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/siwananda/sampledockermaven/SampleDockerMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package siwananda.sampledockermaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleDockerMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
